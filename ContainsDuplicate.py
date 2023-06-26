b68face7dc40fb76cf10c18501c0262dae1c53bb
RohanKumariya <80073509+RohanKumariya@users.noreply.github.com>
2023-06-26 12:17:55 -0500

    Update ContainsDuplicate.py

--- ContainsDuplicate.py
+++ ContainsDuplicate.py
@@ -2,19 +2,20 @@ class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
 
         #Initialize a hashset to find distinct values
-				hashset = set()
+	hashset = set()
 
-				#Initializing the loop for finding distinct Values
-				for i in nums:
-					
-						#Condition to check whether the number is already in the list if it is we return True
-				    if i in hashset:
-						    return True
-						#If the value is distinct and not present in the list. We add the number to the hashset
-						hashset.add(i)
+	#Initializing the loop for finding distinct Values
+	for i in nums:
+		
+		#Condition to check whether the number is already in the list if it is we return True
+		if i in hashset:
+			return True
+			
+		#If the value is distinct and not present in the list. We add the number to the hashset
+		hashset.add(i)
 
-				#If all values are unique we return False
-				return False
+	#If all values are unique we return False
+	return False
 			
 				
         
